2c0dce9792e9f71fe38d77312b5408514328bf55
hardikdesai01 <109784831+hardikdesai01@users.noreply.github.com>
2022-08-26 13:48:01 +0530

    Update Circle1.java

--- Circle1.java
+++ Circle1.java
@@ -2,12 +2,12 @@ package com.proj1;
 public class Circle1 {
 	private static int count;  
 	float center,radius;
-	public Circle(float c,float r) {
+	public Circle1(float c,float r) {
 		this.center=c;
 		this.radius=r;
 		count++;
 	}
-	Circle(){
+	Circle1(){
 		System.out.println("this a default constructor");  
 		count++;
 	}
@@ -15,12 +15,12 @@ void display() {
 	System.out.println("Center:"+center+" "+"radius:"+radius);
 }
 	public static void main(String[] args) {
-		Circle c1=new Circle(11.2f,8.9f);
-		Circle c2=new Circle(15.0f,38.9f);
+		Circle1 c1=new Circle1(11.2f,8.9f);
+		Circle1 c2=new Circle1(15.0f,38.9f);
 		c1.display();
 		c2.display();
-		Circle c3=new Circle();
-		System.out.print("Total Number of Instance: " + Circle.count); 
+		Circle1 c3=new Circle1();
+		System.out.print("Total Number of Instance: " + Circle1.count); 
 		
 	}
 
